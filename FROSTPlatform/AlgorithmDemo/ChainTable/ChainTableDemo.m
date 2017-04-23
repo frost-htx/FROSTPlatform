69b8bc21dc1e3be9af87867220be4398d11bd59b
git@frost <303931396@qq.com>
2017-04-23 17:47:22 +0800

    commit

--- FROSTPlatform/AlgorithmDemo/ChainTable/ChainTableDemo.m
+++ FROSTPlatform/AlgorithmDemo/ChainTable/ChainTableDemo.m
@@ -27,8 +27,25 @@ typedef struct Node * ChainList;
     if (self = [super init]) {
         
         ChainList list = InitChainList();
+        Creat_ChainList(list);
+        
+//        Node *newNode = InitNodeData();
+//        ChainListInsertItem(list,newNode,5); //插入元素到链表中
+//        ChianListDeleteItem(list,2); //删除链表中的元素；
+        DeleteChainList(list); //删除整个链表;
+        
     }
     return self;
+}
+
+Node* InitNodeData()
+{
+    Node *nodeData;
+    nodeData = malloc(sizeof(Node));
+    if (nodeData) {
+        nodeData->data = 11;
+    }
+    return nodeData;
 }
 
 ChainList InitChainList ()
@@ -45,7 +62,7 @@ ChainList InitChainList ()
     return chainList;
 }
 
-void Creat_ChainList(Node *list)
+void Creat_ChainList(ChainList list)
 {
     int a = 10;
     
@@ -60,11 +77,70 @@ void Creat_ChainList(Node *list)
 }
 
 
+Node* GetChainListItem(ChainList list,int Index)
+{
+    int j = 0;
+    
+    Node *item = list;
+    
+    while (j < Index && item) {
+        item = item->next;
+        j++;
+    }
+    
+    if (item) {
+        return item;
+    } else {
+        printf("不存在此节点");
+        return NULL;
+    }
+    
+}
 
 
+void ChainListInsertItem(ChainList chainList,Node* newNode,int Index)
+{
+    Node *item = GetChainListItem(chainList, Index);
+    newNode->next = item->next;
+    item->next = newNode;
+    
+}
 
+void ChianListDeleteItem(ChainList chainList,int Index)
+{
+    Node *preItem = chainList;
+    int j = 0;
+    
+    while (j < Index - 1 && preItem) {
+        preItem = preItem->next;
+        j++;
+    }
+    
+    if (preItem) {
+        Node *currentItem = preItem -> next;
+        Node *nextItem = currentItem -> next;
 
+        preItem->next = nextItem;
+        free(currentItem);
+        currentItem = NULL;
+    }
+}
 
-
+void DeleteChainList(ChainList chainList)
+{
+    ChainList p,q;
+    
+    p = chainList ->next;
+    
+    while (p) {
+        
+        q = p ->next;
+        free(p);
+        p = q;
+        
+    }
+    
+    chainList->next = NULL;
+}
 
 @end
